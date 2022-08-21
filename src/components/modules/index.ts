2916ba6d4ffac86defd5d4a4d9b29ef68887fad7
Ivan <asdnbvb123@gmail.com>
2022-08-21 16:43:38 +0300

    finish: complete entry page creation

--- src/components/modules/index.ts
+++ src/components/modules/index.ts
@@ -1,4 +1,7 @@
-import { Footer } from "./Footer/Footer";
-import { Header } from "./Header/Header";
+import { Footer } from './Footer/Footer';
+import { Header } from './Header/Header';
+import { Billboard } from './Billboard/Billboard';
+import { About } from './About/About';
+import { Crew } from './Crew/Crew';
 
-export { Footer, Header }
+export { About, Billboard, Footer, Header, Crew }
