fa69728a59282138bec24790c75ac76618db8eea
Ivan <asdnbvb123@gmail.com>
2022-08-23 19:54:36 +0300

    feat: create main ruler-component

--- src/pages/Games/Savannah/Savannah.tsx
+++ src/pages/Games/Savannah/Savannah.tsx
@@ -1,22 +1,52 @@
-import { Box } from '@mui/material';
 import React, { useState } from 'react';
-import generateId from '../../../utils/randomIdGenerator';
+
+import { Box, Typography } from '@mui/material';
+import { Viewport } from './Viewport';
+import { Settings } from './Settings';
+import { Router } from './Router';
 
 const Savannah = () => {
-  const [isGameStarted, setGameIsStarted] = useState(false);
+  const [difficulty, setDifficulty] = useState('0');
+  const [status, setStatus] = useState('TO_BE_STARTED');
 
-  return <Box></Box>;
+  const onGameStart = (newDifficulty: string) => {
+    setDifficulty(newDifficulty);
+    setStatus('STARTED');
+  };
+
+  const onGameEnd = () => {
+    setStatus('PENDING');
+  };
+
+  const onNextGame = () => {
+    setStatus('TO_BE_STARTED');
+  };
+
+  const onError = () => {
+    setStatus('SERVER_ERROR');
+  };
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        rowGap: '2rem',
+      }}
+    >
+      <Typography variant="h2">Savannah</Typography>
+      {status === 'TO_BE_STARTED' ? (
+        <Settings onGameStart={onGameStart} />
+      ) : status === 'STARTED' ? (
+        <Viewport newDifficulty={difficulty} onGameEnd={onGameEnd} />
+      ) : (
+        <Router onNextGame={onNextGame} />
+      )}
+    </Box>
+  );
 };
 
 export { Savannah };
-
-/**
- * Savannah form + start on form submit
- * Savannah Controls
- * 4 Перевода слова, на каждом висит листенер на нажатие ресет всех слов и
- * триггер определенной анимации, в зависимости от успеха / провала
- * Также useMemo / useState - хранит слова, на которые ответил пользователь
- * Есть жизни пользователя, по их окончанию кончается и игра,
- * Всплывает попап, который предлагает сыграть заного и дает статистику,
- * В controls также передается callback, который ставит стейт идущей игры в false
- */
