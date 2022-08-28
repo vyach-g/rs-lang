1dea86eadd29c5eb0613b1c95c5408c04d6e81d7
vyach-g <vyacheslav.grabovskiy@gmail.com>
2022-08-28 15:57:40 +0600

    feat: add base textbook page

--- src/components/modules/Showcase/Showcase.tsx
+++ src/components/modules/Showcase/Showcase.tsx
@@ -1,3 +1,13 @@
-const Showcase = () => {};
+import React from 'react';
+
+import { Container, Typography } from '@mui/material';
+
+const Showcase: React.FC = () => {
+  return (
+    <Container>
+      <Typography>Витрина с играми скоро будет здесь</Typography>
+    </Container>
+  );
+};
 
 export { Showcase };
--- src/components/modules/WordList/WordList.tsx
+++ src/components/modules/WordList/WordList.tsx
@@ -1,3 +1,87 @@
-const WordList = () => {};
+interface Word {
+  id: string;
+  group: number;
+  page: number;
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  textExampleTranslate: string;
+  textMeaningTranslate: string;
+  wordTranslate: string;
+}
+
+import React, { useEffect, useState } from 'react';
+import storage from '../../../storage/storage';
+import { getWords } from '../../../api/apiCalls';
+import {
+  Container,
+  Pagination,
+  Typography,
+  CircularProgress,
+  Grid,
+  Tab,
+  Tabs,
+  Box,
+} from '@mui/material';
+import { WordCard } from '../../base';
+import { WordDTO } from '../../../api/apiCalls.types';
+import { PAGE_PER_GROUP } from './wordListConsts';
+
+const WordList: React.FC = () => {
+  const [wordList, setWordList] = useState<WordDTO[]>([]);
+  const [group, setGroup] = useState(storage.getItem('textbookGroup') || 1);
+  const [page, setPage] = useState(storage.getItem('textbookPage') || 1);
+
+  useEffect(() => {
+    getWords(group - 1, page - 1)
+      .then((response) => {
+        setWordList(response.data);
+        storage.setItem('textbookGroup', group);
+        storage.setItem('textbookPage', page);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [group, page]);
+
+  return (
+    <Container>
+      <Box>
+        <Tabs
+          sx={{ mb: 2, px: 3 }}
+          centered
+          value={group}
+          onChange={(event: React.SyntheticEvent, value: number) => setGroup(value)}
+        >
+          <Tab label="Group 1" value={1} />
+          <Tab label="Group 2" value={2} />
+          <Tab label="Group 3" value={3} />
+        </Tabs>
+      </Box>
+      <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center">
+        {!!wordList.length && (
+          <>
+            {wordList.map((word) => {
+              return <WordCard key={word.id} {...word}></WordCard>;
+            })}
+            <Pagination
+              count={PAGE_PER_GROUP}
+              page={page}
+              onChange={(event, num) => setPage(num)}
+              siblingCount={2}
+              color="primary"
+            />
+          </>
+        )}
+        {!wordList.length && <CircularProgress />}
+      </Grid>
+    </Container>
+  );
+};
 
 export { WordList };
