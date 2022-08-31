d9cc899360462b59f572cd558a4ea43b6224c1ef
vyach-g <vyacheslav.grabovskiy@gmail.com>
2022-08-31 23:27:20 +0600

    feat: add new pagination

--- src/components/modules/WordList/WordList.tsx
+++ src/components/modules/WordList/WordList.tsx
@@ -4,7 +4,13 @@ import { useAuthContext } from '../../../context/AuthContextProvider';
 import storage from '../../../storage/storage';
 import { getWords, getUserAggregatedWords } from '../../../api/apiCalls';
 import { UserAggregatedWord, UserAggregatedWords } from '../../../api/apiCalls.types';
-import { PAGE_PER_GROUP, TextbookTab, WORDS_TOTAL, WORD_PER_PAGE } from './wordListConsts';
+import {
+  GROUP_COLORS,
+  PAGE_PER_GROUP,
+  TextbookTab,
+  WORDS_TOTAL,
+  WORD_PER_PAGE,
+} from './wordListConsts';
 
 import { WordCard } from '../../base';
 import {
@@ -15,6 +21,9 @@ import {
   Grid,
   Tab,
   Tabs,
+  Box,
+  Card,
+  CardMedia,
 } from '@mui/material';
 
 type WordListProps = {
@@ -96,6 +105,11 @@ const WordList: React.FC<WordListProps> = (props) => {
     }
   }, [group, page]);
 
+  const removeFromHard = (num: number) => {
+    const newHardWordList = [...wordList.slice(0, num), ...wordList.slice(num + 1)];
+    setWordList(newHardWordList);
+  };
+
   //unrealized functionality of learned pages, TODO
   useEffect(() => {
     if (auth && page !== TextbookTab.Hard) {
@@ -113,40 +127,74 @@ const WordList: React.FC<WordListProps> = (props) => {
         setLearnedPages(learned);
       });
     }
-  }, [group]);
+  }, [group, page]);
   //
 
   return (
-    <Container sx={{ mb: 2 }}>
+    <Container sx={{ py: 2 }}>
       <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center">
         <Container sx={{ display: 'flex' }}>
           <Tabs
-            sx={{ mb: 2, mx: 'auto' }}
-            centered
-            variant="scrollable"
+            sx={{ mb: 2, mx: 'auto', width: '850px', gap: '10px' }}
+            variant="fullWidth"
             value={group}
             onChange={(event: React.SyntheticEvent, value: number) => {
-              setGroup(value);
-              setPage(1);
-              setWordList([]);
+              if (group !== value) {
+                setIsLoading(true);
+                setWordList([]);
+                setGroup(value);
+                setPage(1);
+                setLearnedPages([]);
+              }
             }}
           >
-            <Tab disabled={isLoading ? true : false} label="A1" value={TextbookTab.A1} />
-            <Tab disabled={isLoading ? true : false} label="A2" value={TextbookTab.A2} />
-            <Tab disabled={isLoading ? true : false} label="B1" value={TextbookTab.B1} />
-            <Tab disabled={isLoading ? true : false} label="B2" value={TextbookTab.B2} />
-            <Tab disabled={isLoading ? true : false} label="C1" value={TextbookTab.C1} />
-            <Tab disabled={isLoading ? true : false} label="C2" value={TextbookTab.C2} />
+            <Tab
+              disabled={isLoading ? true : false}
+              label="A1"
+              value={TextbookTab.A1}
+              sx={{ borderRadius: 3, backgroundColor: GROUP_COLORS[TextbookTab.A1 - 1].light }}
+            />
+            <Tab
+              disabled={isLoading ? true : false}
+              label="A2"
+              value={TextbookTab.A2}
+              sx={{ borderRadius: 3, backgroundColor: GROUP_COLORS[TextbookTab.A2 - 1].light }}
+            />
+            <Tab
+              disabled={isLoading ? true : false}
+              label="B1"
+              value={TextbookTab.B1}
+              sx={{ borderRadius: 3, backgroundColor: GROUP_COLORS[TextbookTab.B1 - 1].light }}
+            />
+            <Tab
+              disabled={isLoading ? true : false}
+              label="B2"
+              value={TextbookTab.B2}
+              sx={{ borderRadius: 3, backgroundColor: GROUP_COLORS[TextbookTab.B2 - 1].light }}
+            />
+            <Tab
+              disabled={isLoading ? true : false}
+              label="C1"
+              value={TextbookTab.C1}
+              sx={{ borderRadius: 3, backgroundColor: GROUP_COLORS[TextbookTab.C1 - 1].light }}
+            />
+            <Tab
+              disabled={isLoading ? true : false}
+              label="C2"
+              value={TextbookTab.C2}
+              sx={{ borderRadius: 3, backgroundColor: GROUP_COLORS[TextbookTab.C2 - 1].light }}
+            />
             {auth && (
               <Tab
                 disabled={isLoading ? true : false}
                 label="Сложные слова"
                 value={TextbookTab.Hard}
+                sx={{ borderRadius: 3, backgroundColor: GROUP_COLORS[TextbookTab.Hard - 1].light }}
               />
             )}
           </Tabs>
         </Container>
-        {group !== TextbookTab.Hard && (
+        {/* {group !== TextbookTab.Hard && (
           <Pagination
             disabled={isLoading ? true : false}
             count={PAGE_PER_GROUP}
@@ -159,20 +207,157 @@ const WordList: React.FC<WordListProps> = (props) => {
             color="primary"
             sx={{ mb: 2 }}
           />
+        )} */}
+        {/* <Tabs
+          value={page}
+          sx={{ mb: 2 }}
+          variant="scrollable"
+          onChange={(event, num) => {
+
+          }}
+        >
+          {Array(30)
+            .fill('')
+            .map((elem, index) => {
+              return (
+                <Tab
+                  value={index}
+                  label={index + 1}
+                  key={index}
+                  sx={{
+                    width: '20px',
+                    minWidth: '20px',
+                    backgroundColor: GROUP_COLORS[group].light,
+                  }}
+                ></Tab>
+              );
+            })}
+        </Tabs> */}
+
+        {group !== TextbookTab.Hard && (
+          <Box
+            sx={{
+              width: '100%',
+              maxWidth: '850px',
+              overflowX: { xs: 'scroll', md: 'hidden' },
+              // overflowX: 'hidden',
+
+              mb: 2,
+            }}
+          >
+            <Box sx={{ width: '850px' }}>
+              {Array(30)
+                .fill('')
+                .map((elem, index) => {
+                  return (
+                    <Box
+                      component="span"
+                      key={index}
+                      sx={{
+                        width: '56.5px',
+                        height: '56px',
+                        display: 'inline-flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+
+                        // minWidth: '20px',
+                      }}
+                    >
+                      <Box
+                        sx={{
+                          height: '52px',
+                          width: '52px',
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          borderRadius: '5px',
+                          transition: 'all 0.3s',
+                          cursor: isLoading ? 'inherit' : 'pointer',
+                          boxSizing: 'border-box',
+                          backgroundColor: isLoading
+                            ? '#E3E3E3'
+                            : index === page - 1
+                            ? GROUP_COLORS[group - 1].dark
+                            : GROUP_COLORS[group - 1].light,
+                          border:
+                            learnedPages[index] || index === page - 1
+                              ? '4px solid rgba(0, 0, 0, .5)'
+                              : '',
+                          fontWeight: 'bold',
+                          color: index === page - 1 ? '#ffffff' : '#000000',
+                          '&:hover': {
+                            backgroundColor: isLoading ? 'E3E3E3' : '#ffffff',
+                            color: '#000000',
+                          },
+                        }}
+                        onClick={() => {
+                          if (page - 1 !== index && !isLoading) {
+                            setIsLoading(true);
+                            setWordList([]);
+                            setPage(index + 1);
+                          }
+                        }}
+                      >
+                        {index + 1}
+                      </Box>
+                    </Box>
+                  );
+                })}
+            </Box>
+          </Box>
         )}
-        {auth && learnedWords.reduce((prev, curr) => prev && !!curr, true) && (
-          <Typography variant="h5" align="center">
-            На данной странице изучены все слова
-          </Typography>
-        )}
+        {/* {auth &&
+          learnedWords.reduce((prev, curr) => prev && !!curr, true) &&
+          group === TextbookTab.Hard &&
+          !isLoading && (
+            <Typography variant="h5" align="center">
+              На данной странице изучены все слова
+            </Typography>
+          )} */}
         {
           <>
-            {wordList.map((word) => {
-              return <WordCard key={word._id} {...word}></WordCard>;
+            {wordList.map((word, index) => {
+              return (
+                <WordCard
+                  removeFromHard={removeFromHard}
+                  isHardGroup={group === TextbookTab.Hard}
+                  index={index}
+                  key={word._id}
+                  {...word}
+                ></WordCard>
+              );
             })}
           </>
         }
-        {isLoading && <CircularProgress color="primary" />}
+        {
+          isLoading &&
+            Array(20)
+              .fill('')
+              .map((elem, index) => {
+                return (
+                  <Container key={index} maxWidth="md">
+                    <Card
+                      sx={{
+                        display: { sm: 'flex' },
+                        // width: '852px',
+                        height: { xs: '510px', sm: '186px' },
+                        backgroundColor: '#F3F3F3',
+                        borderRadius: 3,
+                        marginBottom: '30px',
+                        boxShadow: '0 4px 10px rgba(0,0,0,0.08)',
+                      }}
+                    >
+                      <CardMedia
+                        sx={{ width: { sm: 200 }, minHeight: 186, backgroundColor: '#E3E3E3' }}
+                        component="span"
+                      />
+                    </Card>
+                  </Container>
+                );
+              })
+
+          // <CircularProgress color="primary" />
+        }
       </Grid>
     </Container>
   );
--- src/components/modules/WordList/wordListConsts.ts
+++ src/components/modules/WordList/wordListConsts.ts
@@ -1,3 +1,5 @@
+import { blue, lightBlue, lightGreen, lime, orange, red, yellow } from '@mui/material/colors';
+
 export const GROUP_TOTAL = 6;
 export const PAGE_PER_GROUP = 30;
 export const WORD_PER_PAGE = 20;
@@ -12,3 +14,13 @@ export enum TextbookTab {
   C2,
   Hard,
 }
+
+export const GROUP_COLORS = [
+  { dark: blue[700], light: blue[100] },
+  { dark: lightBlue[700], light: lightBlue[100] },
+  { dark: lightGreen[700], light: lightGreen[100] },
+  { dark: lime[700], light: lime[100] },
+  { dark: yellow[700], light: yellow[100] },
+  { dark: orange[700], light: orange[100] },
+  { dark: red[700], light: red[100] },
+];
